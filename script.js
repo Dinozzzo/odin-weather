60600fecb45c2f06028a1f54295780bf476ec158
Dinozzzo <dleootta@gmail.com>
2026-04-03 23:20:56 +0200

    page is ready, need to code the API and display it on the screen

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 async function getWeather() {
   try {
-    const response = await fetch();
+    const response = await fetch(
+      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=ZCVASQT97QAJ8KLBMZ7PMRBYX",
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
   } catch (error) {
     console.log(error);
   }
